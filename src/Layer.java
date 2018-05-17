b15bb976cf004ea438554c0227804638e78545bc
IbrahimSyedIS <ibrahim0m0syed@gmai.com>
2018-05-17 18:23:56 -0400

    Fixing Stupid Neural Network

--- src/Layer.java
+++ src/Layer.java
@@ -2,30 +2,26 @@ import java.util.ArrayList;
 
 public class Layer {
 
-    private ArrayList<Neuron> neurons;
+    // ArrayList representing all the neurons in this instantiated layer
+    ArrayList<Neuron> neurons;
 
+    // Initializing the neurons ArrayList
     public Layer() {
         neurons = new ArrayList<>();
     }
 
-    public void addNeuron(Neuron neuron) {
-        neurons.add(neuron);
+    // Adding a neuron to this layer
+    public void addNeuron(Neuron n) {
+        neurons.add(n);
     }
 
-    public void connectLayer(Layer layer) {
-        for (Neuron neuron : neurons) {
-            for (Neuron otherNeuron : layer.neurons) {
-                otherNeuron.connectNeuron(neuron);
+    // Connecting all the neurons in this layer to the neurons in the next one
+    public void connectToLayer(Layer layer) {
+        for (Neuron n1 : neurons) {
+            for (Neuron n2 : layer.neurons) {
+                n1.connectWeight(n2);
             }
         }
-    }
-
-    public double[] computeValues() {
-        double[] values = new double[neurons.size()];
-        for (int i = 0; i < neurons.size(); i++) {
-            values[i] = neurons.get(i).computeValue();
-        }
-        return values;
     }
 
 }
--- src/Neuron.java
+++ src/Neuron.java
@@ -3,26 +3,48 @@ import java.util.Map;
 
 public class Neuron {
 
-    private LinkedHashMap<Neuron, Double> inputs;
+    // Linked Hash Map representing the next layer of neurons and the associated weights
+    private LinkedHashMap<Neuron, Double> nextLayer;
 
+    // The sum accumulated from the neurons in the last layer
+    private double weightedSum;
+
+    // The weighted sum but with the activation function applied
+    private double valueToSend;
+
+    // Initializing all the variables
     public Neuron() {
-        inputs = new LinkedHashMap<>();
+        nextLayer = new LinkedHashMap<>();
+        weightedSum = 0;
+        valueToSend = 0;
     }
 
+    // Adding a neuron from the next layer to this neurons connections
+    public void connectWeight(Neuron n) {
+        nextLayer.put(n, Math.random());
+    }
+
+    // Taking a value from the previous layer and adding it to the weighted sum
+    public void addToWeightedSum(double value) {
+        weightedSum += value;
+    }
+
+    // Applying the activation function to the weighted sum and then resetting it
+    public void activate() {
+        valueToSend = sigmoid(weightedSum);
+        weightedSum = 0;
+    }
+
+    // Sending the activated value to the neurons in the next layer multiplied by the associated weights
+    public void fire() {
+        for (Map.Entry<Neuron, Double> entry : nextLayer.entrySet()) {
+            entry.getKey().addToWeightedSum(valueToSend * entry.getValue());
+        }
+    }
+
+    // Sigmoid activation function
     private double sigmoid(double x) {
         return 1d / (1 + Math.exp(-x));
-    }
-
-    public void connectNeuron(Neuron incomingNeuron) {
-        inputs.put(incomingNeuron, Math.random());
-    }
-
-    public double computeValue() {
-        double weightedSum = 0.0;
-        for (Map.Entry<Neuron, Double> entry : inputs.entrySet()) {
-            weightedSum += entry.getKey().computeValue() * entry.getValue();
-        }
-        return sigmoid(weightedSum);
     }
 
 }
